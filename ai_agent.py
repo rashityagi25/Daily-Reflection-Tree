e05f5fe602202326dd27a7a10da749fd02c4f799
Rashi Tyagi <rashityagi2504@gmail.com>
2026-04-27 10:46:16 +0530

    Add ai_agent function for user emotional support

--- ai_agent.py
+++ ai_agent.py
@@ -1,1 +1,8 @@
 
+def ai_agent(user_input):
+    if "sad" in user_input:
+        return "It seems you're feeling sad. Try journaling your thoughts."
+    elif "stress" in user_input:
+        return "Take a deep breath and list your top 3 concerns."
+    else:
+        return "Reflect on your day and write key learnings."
